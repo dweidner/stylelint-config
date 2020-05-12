fd32baf2f6c19fad6484a3e23404e9213252c0be
Daniel Weidner <hallo@danielweidner.de>
2020-05-12 13:41:55 +0200

    feat!: less strict rules for selectors
    In order to allow the following selectors the
    rules applied to css selectors are changed:
    
    ```css
    blockquote > p {} /* selector-max-type */
    .no-touch .c-heading:hover .c-heading-anchor {} /* selector-max-class */
    ```

--- config/selector.js
+++ config/selector.js
@@ -4,7 +4,7 @@ module.exports = {
   // Require quotes for attribute values.
   'selector-attribute-quotes': 'always',
   // Limit the number of classes in a selector.
-  'selector-max-class': 2,
+  'selector-max-class': 3,
   // Limit the number of combinators in a selector.
   'selector-max-combinators': 1,
   // Limit the number of compound selectors in a selector.
@@ -12,9 +12,9 @@ module.exports = {
   // Limit the number of ID selectors in a selector.
   'selector-max-id': 0,
   // Limit the specificity of selectors.
-  'selector-max-specificity': '0,3,0',
+  'selector-max-specificity': '0,4,0',
   // Limit the number of type selectors in a selector.
-  'selector-max-type': 1,
+  'selector-max-type': 2,
   // Limit the number of universal selectors in a selector.
   'selector-max-universal': 1,
   // Disallow qualifying a selector by type.
